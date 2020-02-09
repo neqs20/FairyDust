99944dd3837de4d9122912d78e4b31a2dfd04404
HazmatDemon <michael0march@gmail.com>
2020-02-09 20:27:56 +0100

    added simple Third-Person Camera like in shaiya

--- Scripts/tools/collision_shape_generator.gd
+++ Scripts/tools/collision_shape_generator.gd
@@ -4,4 +4,4 @@ extends MeshInstance
 # Then this .tres file can be dragged to CollisionShape in the editor (Inspector Tab) 
 
 func _ready():
-	ResourceSaver.save("res://terrain-shape.tres", mesh.create_convex_shape())
+	ResourceSaver.save("res://terrain-shape.tres", mesh.create_trimesh_shape())
